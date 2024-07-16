19e4a944c79b16266802a069daf0f585c5e0885c
jessvasq <jessnsq.codes@gmail.com>
2024-07-16 11:49:47 -0400

    file structure

---
+++ SECUBANK/src/main/java/com/nvz/secubank/SecubankApplication.java
@@ -0,0 +1,13 @@
+package com.nvz.secubank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecubankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecubankApplication.class, args);
+    }
+
+}
---
+++ SECUBANK/src/test/java/com/nvz/secubank/SecubankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvz.secubank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecubankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
